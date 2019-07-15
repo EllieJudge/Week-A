9dba73380d3a016657243de0ef362f5592265e14
My Super Blog <judge_ellie@hotmail.com>
2019-07-15 20:44:12 +0100

    finished cash machine woohoo

--- CASHMACHINE/day3CashMachine.js
+++ CASHMACHINE/day3CashMachine.js
@@ -3,8 +3,6 @@ let userInput;
 let pin = 1;
 let balance = 20;
 let depositAmount;
-// let withdrawalAmount;
-let newBalance;
 let run = false;
 
 alert("Welcome...")
@@ -13,9 +11,9 @@ alert("Welcome...")
 function checkPin() {
     userInput = prompt("Please enter your pin: ")
     if (userInput == pin) {
-        console.log("Pin Correct")
         alert("Pin correct.")
         run = true;
+        menuOptions()
     }
     else{
         alert("Pin incorrect. Get lost. Fraud.")
@@ -46,13 +44,15 @@ function checkBalanceFunc() {
     if (userInput == 1) {
         menuOptions()
     } else if (userInput == 2) {
-        alert("Goodbye")
+        alert("Oh sod off, then.")
+        return run = false;
     } else {
-        alert("Does not compute. Try again?")
+        alert("Does not compute. Try again??")
         menuOptions()
     }
 }
 
+//withdraw suggested amount or enter own
 function withdrawMoolaFunc() {
     userInput = prompt(`How much would you like to withdraw today? \n1. £5  \n2. £10 \n3. £20 \n4. £50 \n5. Other`)
     switch (userInput) {
@@ -68,24 +68,40 @@ function withdrawMoolaFunc() {
         case "4":
         withdrawalSum(50)
         break;
+        case"5":
+        userInput = prompt("Enter amount: ")
+        withdrawalSum(userInput)
+        break;
 
     }
 }
 
+//calculates balance - withdrawal amount, exits if not enough moola
 function withdrawalSum(moola) {
     balance = balance - moola
-
     if (balance < 0) {
-        prompt("You dont have enough money to withdraw that amount. Would you like another service? \n1. Yes \n2. No")
-        return
+        alert("You dont have enough money to withdraw that amount, kid. Sorry.")
+       return run = false
     }
     alert(`You have withdrawn £${moola}. Your new balance is £${balance}`)
+    withdrawMoolaFunc()
 }
 
+//add moola to your account
 function makeDepositFunc() {
-
+    userInput = prompt('How much would you like to deposit today?')
+    balance = +balance + +userInput
+    alert (`Your new balance is £${balance}`)
 }
 
+//enter pin to change pin
 function changePinFunc() {
-
+    userInput = prompt("To change pin, enter current pin")
+    if (userInput==pin) {
+        userInput = prompt("Now enter your new pin")
+        pin = userInput;
+        console.log(pin)
+        checkPin()
+    }
+    else alert("Not authroised!!")
 }
